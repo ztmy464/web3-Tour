a9177f96a9a83c8635a4d0cd89927f86a6c01da8
ztmy464 <ztmy464@gmail.com>
2025-03-03 23:14:12 +0800

    commit

---
+++ foundations/tools/foundry/DAO-f23/src/Box.sol
@@ -0,0 +1,26 @@
+// contracts/Box.sol
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.8.0;
+
+import {Ownable} from "@openzeppelin/contracts/access/Ownable.sol";
+
+contract Box is Ownable {
+    uint256 private value;
+
+    // Emitted when the stored value changes
+    event ValueChanged(uint256 newValue);
+
+    constructor(address initialOwner) {
+        initialOwner = msg.sender;
+    }
+    // Stores a new value in the contract
+    function store(uint256 newValue) public onlyOwner {
+        value = newValue;
+        emit ValueChanged(newValue);
+    }
+
+    // Reads the last stored value
+    function retrieve() public view returns (uint256) {
+        return value;
+    }
+}
